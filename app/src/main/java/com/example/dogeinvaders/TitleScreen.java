ddb299fb0abc080a4d9d46465fe530dbc58c0335
Vysakh <vysakhav951@gmail.com>
2021-02-12 15:33:38 +0530

    Added Title Screen

---
+++ app/src/main/java/com/example/dogeinvaders/TitleScreen.java
@@ -0,0 +1,22 @@
+package com.example.dogeinvaders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+
+    public void goToDogeInvaders(View view)
+    {
+        Intent dogeInvadersScreen = new Intent(this, DogeInvadersActivity.class);
+        startActivity(dogeInvadersScreen);
+    }
+}
